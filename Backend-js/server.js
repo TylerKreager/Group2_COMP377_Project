7ecdcdff254d60a0dded6031c47e93df02900a8b
Tyler Kreager <kreagertyler@gmail.com>
2026-04-13 09:43:57 -0400

    Added cors

--- Backend-js/server.js
+++ Backend-js/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import fileUpload from "express-fileupload";
+import cors from "cors";
 import classifyImage from "./routes/classify.js";
 
 const app = express();
+app.use(cors());
 app.use(fileUpload());
 app.use(express.json());
 
